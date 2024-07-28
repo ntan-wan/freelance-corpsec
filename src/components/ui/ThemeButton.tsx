023ab811197272dbcd31dcf1f5d68420eecc3eca
wang <wangtanngan@gmail.com>
2024-07-28 19:27:25 +0800

    [FEAT] dark mode logic

--- src/components/ui/ThemeButton.tsx
+++ src/components/ui/ThemeButton.tsx
@@ -1,17 +1,26 @@
 "use client";
 
 import { CustomButton } from "@/components/ui/CustomButton";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Theme } from "@/types/coreTypes";
 
 export function ThemeButton() {
     const [theme, setTheme] = useState<Theme>("light");
+
+	useEffect(() => {
+		if (localStorage.getItem('theme') == 'dark') {
+        	document.documentElement.classList.add('dark');
+			setTheme('dark');
+		}
+	});
     const handleClickTheme = () => {
 		if (theme == 'light') {
         	document.documentElement.classList.add('dark');
+			localStorage.setItem('theme', 'dark');
 			setTheme('dark');
 		} else if (theme == 'dark') {
         	document.documentElement.classList.remove('dark');
+			localStorage.removeItem('theme');
 			setTheme('light');
 		}
     };
